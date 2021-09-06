b3563cc2d140a42af1b72b631cb8003d1de8645e
Benoit Pingris <benoit.pingris@pm.me>
2021-09-06 22:20:12 +0200

    commit woohoo

---
+++ README.md
@@ -0,0 +1,28 @@
+# Decorators python
+
+## Wraps existing function to add new behaviour
+
+syntax:
+```python
+@decorator
+def my_function(n: int):
+    print("odd" if n % 2 == 0 else "even")
+```
+
+`decorator` is a function with the following body:
+```python
+def decorator(func): # `func` is the decorater function
+
+    # `handler` is the function called when the decorated function is called in the code
+    def handler(*args, **kwargs):
+        # Logic might go here
+        print("Pre-call")
+
+        func(*args, **kwargs) # This is the call to the decorated function, the logic written in the orignal function is not altered
+
+        # or here
+        print("Post-call")
+
+    # We return the new function
+    return handler
+```
---
+++ main.py
@@ -0,0 +1,73 @@
+import time
+
+
+def timeit(func):
+    def handler(*args, **kwargs):
+        s = time.perf_counter()
+        func(*args, **kwargs)
+        e = time.perf_counter()
+        print(f"function took {e-s:.3f}s to run.")
+
+    return handler
+
+def timeit_range(n: int):
+    def handler(func):
+        def _handler(*args, **kwargs):
+            mean = 0
+            for _ in range(n):
+                s = time.perf_counter()
+                func(*args, **kwargs)
+                e = time.perf_counter()
+                mean += e-s
+            print(f"function took {mean/n:.3f}s to run in average.")
+        return _handler
+    return handler
+
+def logit(func):
+    def handler(*args, **kwargs):
+        print(f"About to call {func.__name__}...")
+        func(*args, **kwargs)
+        print("Done.")
+
+    return handler
+
+N=10_000_000
+
+# Function with no decorator
+def some_fn():
+    for _ in range(N):
+        pass
+
+# Function with 1 simple decorator
+# @time
+# def some_fn():
+#     for _ in range(N):
+#         pass
+
+# Function with 1 parametable decorator
+# @timeit_range(10)
+# def some_fn():
+#     for _ in range(N):
+#         pass
+
+# Function with 1 simple decorator
+# @logit
+# def some_fn():
+#     for _ in range(N):
+#         pass
+
+# Function with 2 decorators
+# @logit
+# @timeit_range(10)
+# def some_fn():
+#     for _ in range(N):
+#         pass
+
+# Function with 2 decorators, in another order
+# @timeit_range(10)
+# @logit
+# def some_fn():
+#     for _ in range(N):
+#         pass
+
+some_fn()
